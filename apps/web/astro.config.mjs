7a2c33511453da0a9ca845e18126e0e7bae01ac3
Hexaa <me@hexaa.sh>
2026-09-15 22:43:18 +0700

    feat: setup and initialize the platform

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [svelte(), react()]
+});
