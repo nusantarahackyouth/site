4a987fb38d16f6f0e4a02a6adb51a389bdaa0227
Realzzy <82355841+then77@users.noreply.github.com>
2026-09-16 23:54:41 +0700

    feat(site): Add new navbar (indev) feat(deploy): Split fe be deployment

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,11 +4,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 import react from '@astrojs/react';
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
   },
 
   integrations: [svelte(), react()]
