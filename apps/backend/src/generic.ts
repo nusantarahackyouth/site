a0972909362ad7b0a70782101e20e4ddeb8dd795
Hexaa <me@hexaa.sh>
2026-09-17 20:48:46 +0700

    fix(routes): delete generic.ts

--- apps/backend/src/generic.ts
+++
@@ -1,7 +0,0 @@
-import { HonoBase } from "./app";
-
-export const router = new HonoBase().get("/", (c) => {
-  return c.json({
-    message: "hackyouth.id is literally the same as nusantara hack youth, lmao",
-  });
-});
--- apps/backend/src/routes.ts
+++ apps/backend/src/routes.ts
@@ -1,8 +1,11 @@
 import { HonoBase } from "./app";
-import { router as genericRouter } from "./generic";
 import { router as v1Router } from "./v1/routes";
 
 export const router = new HonoBase()
-  .route("/", genericRouter)
+  .get("/", (c) => {
+    return c.json({
+      message: "hackyouth.id is literally the same as nusantara hack youth, lmao",
+    });
+  })
   .route("/v1", v1Router);
 export type RouterRoutes = typeof router;
