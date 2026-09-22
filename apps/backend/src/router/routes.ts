d9d95345f1384f3e6f6f1bd07e16bc1c76f2ee65
Realzzy <82355841+then77@users.noreply.github.com>
2026-09-22 15:40:32 +0700

    fix(be/routes): Move welcome routes to /api instead fix(be/handler): Add custom not found and error handler

--- apps/backend/src/router/routes.ts
+++ apps/backend/src/router/routes.ts
@@ -2,12 +2,33 @@ import { createHono } from "@/router/app";
 import { router as v1Router } from "@/router/v1/routes";
 import type { Hono } from "hono";
 
-
 export const router = createHono()
-  .get("/", (c) => {
-    return c.json({
-      message: "hackyouth.id is literally the same as nusantara hack youth, lmao",
-    });
+  .get("/", (c) =>
+    c.json({
+      success: true,
+      message: "Welcome to Nusantara Hack Youth public API.",
+      hint: "Go to /api/docs or /api/openapi.json for more info on available endpoints.",
+    }),
+  )
+  .notFound((c) =>
+    c.json(
+      {
+        success: false,
+        message: "Route not found.",
+        hint: "Go to /api/docs or /api/openapi.json for more info on available endpoints.",
+      },
+      404,
+    ),
+  )
+  .onError((err, c) => {
+    // TODO: Log error
+    return c.json(
+      {
+        success: false,
+        message: "Internal server error.",
+      },
+      500,
+    );
   })
   .route("/v1", v1Router);
 
--- apps/backend/src/router/v1/routes.ts
+++ apps/backend/src/router/v1/routes.ts
@@ -3,12 +3,5 @@ import { createHono } from "@/router/app";
 import eventsRouter from "./(events)";
 
 export const router = createHono()
-    .get("/", async (c) => {
-        return c.json({
-            success: true,
-            message: "Welcome to Nusantara Hack Youth public API.",
-            hint: "Go to /api/docs for more info on available endpoints."
-        });
-    })
     .route("/events", eventsRouter);
 export type RouterRoutes = typeof router;
