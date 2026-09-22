49c4cf9f643156386f8b480b7317de4a56380860
Realzzy <82355841+then77@users.noreply.github.com>
2026-09-22 16:49:47 +0700

    fix(be): Fix hono not found & error handler placement + disable strict mode

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,9 +4,17 @@ import { openAPIRouteHandler } from "hono-openapi";
 import { createHono } from "@/router/app";
 import { router } from "@/router/routes";
 
-const api = createHono().basePath("/api").route("/", router);
+const api = createHono({ strict: false }).basePath("/api").route("/", router);
 
 export const app = api
+  .get("/", (c) =>
+    c.json({
+      success: true,
+      message: "Welcome to Nusantara Hack Youth public API.",
+      hint: "Go to /api/docs or /api/openapi.json for more info on available endpoints.",
+    }),
+  )
+
   .get("/openapi.json", (c, next) =>
     openAPIRouteHandler(router, {
       documentation: {
@@ -25,7 +33,27 @@ export const app = api
       pageTitle: "Nusantara Hack Youth API Reference",
       url: "/api/openapi.json",
     }),
-  );
+  )
+  .notFound((c) =>
+    c.json(
+      {
+        success: false,
+        message: "Route not found.",
+        hint: "Go to /api/docs or /api/openapi.json for more info on available endpoints.",
+      },
+      404,
+    ),
+  )
+  .onError((err, c) => {
+    // TODO: Log error
+    return c.json(
+      {
+        success: false,
+        message: "Internal server error.",
+      },
+      500,
+    );
+  });
 
 export type AppType = typeof app;
 export type { RouterRoutes } from "./router/routes";
